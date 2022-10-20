aed310323710ed9eef40061dd8ee879196a2450a
arthurtaverny <arthurtaverny@hotmail.com>
2022-10-19 21:56:27 -0300

    Auto preenchimento implementado.

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ function setCardType(type) {
   ccBgcolor01.setAttribute("fill", colors[type][0])
   ccBgcolor02.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `cc-${type}.svg`)
-  ccBg.style.backgroundColor = colors[type][2]
 }
 
 const securityCode = document.querySelector("#security-code")
@@ -46,3 +45,108 @@ const expirationDatePattern = {
   },
 }
 const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.querySelector("#card-number")
+
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4[0-9]{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^((5(([1-2]|[4-5])[0-9]{0,8}|0((1|6)([0-9]{0,7}))|3(0(4((0|[2-9])[0-9]{0,5})|([0-3]|[5-9])[0-9]{0,6})|[1-9][0-9]{0,7})))|((508116)\\d{0,10})|((502121)\\d{0,10})|((589916)\\d{0,10})|(2[0-9]{0,15})|(67[0-9]{0,14})|(506387)\\d{0,10})/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{0,2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+      cardtype: "cielo",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^3[47][0-9]{0,13}/,
+      cardtype: "amex",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^606282|^3841(?:[0|4|6]{0,1})0/,
+      cardtype: "hipercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const cardInfo = dynamicMasked.compiledMasks.find(({ regex }) =>
+      number.match(regex)
+    )
+
+    return cardInfo
+  },
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+//Função de clique do botão
+
+const addButton = document.querySelector("#add-card")
+
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado com sucesso!")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+//Preenchimento auto do cvc
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+
+  ccSecurity.innerText = code.length === 0 ? 123 : code
+}
+
+//Auto preenchimento do número do cartão
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
+
+//Auto preenchimento da data de expiração
+
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-extra .value")
+  ccExpiration.innerText = date.length === 0 ? "02/32" : date
+}
